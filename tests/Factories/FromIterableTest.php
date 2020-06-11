9a67b56d3739eca424bddf1edca8fb88017b7970
Voltra <ludwig.guerin.98@gmx.fr>
2020-06-11 23:42:28 +0200

    More tests

--- tests/Factories/FromIterableTest.php
+++ tests/Factories/FromIterableTest.php
@@ -6,6 +6,7 @@ namespace LazyCollection\Tests\Factories;
 
 use LazyCollection\Stream;
 use LazyCollection\Tests\PHPUnit;
+use stdClass;
 
 class FromIterableTest extends PHPUnit {
 	/**
@@ -20,5 +21,87 @@ class FromIterableTest extends PHPUnit {
 		}
 
 		self::assertTrue(true);
+	}
+
+	/**
+	 * @test size of stream is size of iterable
+	 * @covers Stream::fromIterable
+	 * @dataProvider provideIterableWithSize
+	 */
+	public function sizeOfStreamIsSizeOfIterable(iterable $it, int $size){
+		$stream = Stream::fromIterable($it);
+		$ssize = 0;
+		foreach($stream as $_){
+			++$ssize;
+		}
+		self::assertSame($size, $ssize);
+	}
+
+	/**
+	 * @test stream does not change keys
+	 * @covers Stream::fromIterable
+	 * @dataProvider provideAssociative
+	 */
+	public function streamDoesNotChangeKeys(array $arr){
+		$expectedKeys = array_keys($arr);
+		$stream = Stream::fromIterable($arr);
+		$keys = [];
+		foreach ($stream as $key => $value){
+			$keys[] = $key;
+		}
+		self::assertSame($expectedKeys, $keys);
+	}
+
+	/**
+	 * @test can create from various iterables
+	 * @covers Stream::fromIterable
+	 * @dataProvider provideDifferentIterables
+	 */
+	public function canCreateFromVariousIterables($it){
+		Stream::fromIterable($it);
+		self::assertTrue(true);
+	}
+
+	public function provideIterableWithSize(): array {
+		return [
+			[
+				[], 0
+			],
+			[
+				[1,2], 2
+			],
+			[
+				[1], 1
+			],
+			[
+				[1,2,3,4,5], 5
+			]
+		];
+	}
+
+	public function provideAssociative(): array{
+		return [
+			[
+				["key" => "value"],
+			],
+			[
+				["key" => "value", "test" => "test"]
+			],
+
+			[
+				["key" => "value", "test" => "test", "value" => "key"]
+			],
+		];
+	}
+
+	public function provideDifferentIterables(): array{
+		return [
+			[
+				["key" => "value"],
+			],
+			[
+				[1,2,3]
+			],
+		];
 	}
 }
